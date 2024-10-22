17f3467858349a191f03aea2fbd56b9f22b654ca
Sai Pavan Kamma <saipavankamma@ufl.edu>
2024-10-22 14:38:04 -0400

    Use the correct link in help.php

--- help.php
+++ help.php
@@ -2,7 +2,7 @@
 
 // have construction emoji
 echo "<h1>🚧 PAGE UNDER CONSTRUCTION 🚧</h1>";
-echo "<h6>Printing setup instructions can be found: <a href='" . $module->getUrl("assets/additional_help.md") . "'>here</a></h6>";
+echo "<h6>Printing setup instructions can be found: <a href='" . $module->getUrl("assets/printing_help.pdf") . "'>here</a></h6>";
 
 
 echo "<embed src='" . $module->getUrl("assets/additional_help.md") . "' width='100%' height='200px' type='application/pdf'>";
